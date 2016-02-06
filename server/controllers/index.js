6dad93b8b75e64bab9f52d32c1b385fec339b10a
ColinWhitmarsh <Colin.Whitmarsh@gmail.com>
2016-02-05 20:53:47 -0800

    start implementing post route

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,29 @@
 var models = require('../models');
+var mysql = require('mysql');
+
+var dbConnection = mysql.createConnection({
+  user: 'root',
+  password: 'password',
+  database: 'chat'
+});
+
+dbConnection.connect();
+
+
 
 module.exports = {
   messages: {
     get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    post: function (req, res) {  
+      var body = '';
+      req.on('data', function(chunk) {
+        body += chunk;
+      });
+      req.on('end', function(){ 
+        
+      });
+
+     } // a function which handles posting a message to the database
   },
 
   users: {
